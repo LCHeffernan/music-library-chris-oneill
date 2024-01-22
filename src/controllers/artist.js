68ca61b8d5b0df1181deec9063b907deab535816
Chris O'Neill <chris_oslow@hotmail.com>
2024-01-22 17:43:33 +0000

    Reading single artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -25,4 +25,21 @@ const readArtists = async (_, res) => {
   }
 };
 
-module.exports = { createArtist, readArtists };
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      rows: [artist],
+    } = await db.query("SELECT * FROM Artists WHERE id = $1", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = { createArtist, readArtists, getArtistById };
